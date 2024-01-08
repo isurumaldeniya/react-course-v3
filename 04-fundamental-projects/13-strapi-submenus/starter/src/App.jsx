0a614267ac5f7148fec695632cbc1c7bb20bd359
isurumaldeniya <md.isurumaldeniya@gmail.com>
2024-01-08 12:30:34 +1100

    Strapi project completed

--- 04-fundamental-projects/13-strapi-submenus/starter/src/App.jsx
+++ 04-fundamental-projects/13-strapi-submenus/starter/src/App.jsx
@@ -1,4 +1,18 @@
+import { useGlobalContext } from './Context';
+import Hero from './Hero';
+import NavBar from './Navbar';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+  // const { isSidebarOpen } = useGlobalContext();
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </>
+  );
 };
 export default App;
---
+++ 04-fundamental-projects/13-strapi-submenus/starter/src/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content
+          <br />
+          AnyWhere
+        </h1>
+
+        <p>This is a test paragraph</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
